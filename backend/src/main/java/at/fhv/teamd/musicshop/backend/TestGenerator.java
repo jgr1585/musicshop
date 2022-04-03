82080c1d28d684c6fe134fe6d33f32b0371fe135
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2022-04-03 14:03:22 +0200

    more testdata

--- backend/src/main/java/at/fhv/teamd/musicshop/backend/TestGenerator.java
+++ backend/src/main/java/at/fhv/teamd/musicshop/backend/TestGenerator.java
@@ -20,7 +20,7 @@ import java.util.*;
 public class TestGenerator {
 
     // TODO: Test data generation with real data -> Articles, Artists, Mediums, Suppliers
-    // TODO: handle duplicate key vialoation -> songs with same artist; album with more than one song; mediums with same supplier
+    // TODO: handle duplicate key violation -> songs with same artist; album with more than one song; mediums with same supplier
     // TODO: postgres tables name (article_article)?
     private static void generateTestData() {
         EntityManager em = PersistenceManager.getEntityManagerInstance();
@@ -32,8 +32,6 @@ public class TestGenerator {
         mediums1.add(new Medium(BigDecimal.valueOf(22), MediumType.VINYL, Stock.of(Quantity.of(5)), new Supplier("ZYX Music GmbH & Co KG - VINYL", Duration.ofDays(4))));
 
         Artist artistLongLive = new Artist("A$AP Rocky");
-
-        em.persist(artistLongLive);
 
         String mbidLongLive = "ab967b6a-de5c-455b-aad2-8b2f5899b288";
         String labelLongLive = "Sony Records International";
@@ -49,8 +47,6 @@ public class TestGenerator {
                 mbidLongLive,
                 mediums1,
                 Set.of(songLongLive));
-
-        em.persist(songLongLive);
 
         em.persist(albumLongLive);
 
@@ -102,144 +98,99 @@ public class TestGenerator {
 
         //ALBUM 4
         Set<Medium> mediums4 = new HashSet<>();
-        mediums4.add(new Medium(BigDecimal.valueOf(19), MediumType.CD, Stock.of(Quantity.of(17)), new Supplier("ZYX Music GmbH & Co KG", Duration.ofDays(1))));
+        mediums4.add(new Medium(BigDecimal.valueOf(13), MediumType.CD, Stock.of(Quantity.of(7)), new Supplier("ZYX Music GmbH & Co KG", Duration.ofDays(6))));
 
-//        //ALBUM 2
-//        Artist artistUntamedDesire = new Artist("50 Cent");
-//
-//        em.persist(artistUntamedDesire);
-//
-//        String mbidUntamedDesire = "b4cfbcbc-b98c-4695-94c9-e095412e4a84";
-//        String labelUntamedDesire = "G-Unit Records";
-//        String genreUntamedDesire= "hip hop, rap";
-//        LocalDate releaseUntamedDesire = LocalDate.of(2014, 6,3);
-//
-//        Song songUntamedDesire = new Song("Hold On", labelUntamedDesire, releaseUntamedDesire, genreUntamedDesire, mbidUntamedDesire, analogMediumMap2, Duration.ofMinutes(3), List.of(artistUntamedDesire));
-//        Album albumUntamedDesire = new Album("Animal Ambition: An Untamed Desire to Win",
-//                labelUntamedDesire,
-//                releaseUntamedDesire,
-//                genreUntamedDesire,
-//                mbidUntamedDesire,
-//                analogMediumMap2,
-//                List.of(songUntamedDesire));
-//
-//        em.persist(songUntamedDesire);
-//        em.persist(albumUntamedDesire);
-//
-//        //ALBUM 3
-//        Artist artistUnion = new Artist("Rasa");
-//
-//        em.persist(artistUnion);
-//
-//        String mbidUnion = "da25e5c4-afcb-4f10-af3f-be2251241b35";
-//        String labelUnion = "Hearts of Space";
-//        String genreUnion = "ambient, electronic, new age, fusion";
-//        LocalDate releaseUnion = LocalDate.of(2001, 1,1);
-//
-//        Song songUnion = new Song("Kabe Habe Bolo", labelUnion, releaseUnion, genreUnion, mbidUnion, analogMediumMap3, Duration.ofMinutes(8), List.of(artistUnion));
-//
-//        Album albumUnion = new Album("Union",
-//                labelUnion,
-//                releaseUnion,
-//                genreUnion,
-//                mbidUnion,
-//                analogMediumMap3,
-//                List.of(songUnion));
-//
-//        em.persist(songUnion);
-//        em.persist(albumUnion);
-//
-//        //ALBUM 4
-//        Artist artistAnti = new Artist("Rihanna");
-//
-//        em.persist(artistAnti);
-//
-//        String mbidAnti = "a84dea70-f81b-4761-a39c-2dd3a9e985cc";
-//        String labelAnti = "Westbury Road Entertainment";
-//        String genreAnti = "contemporary r&b, pop, hip hop, soul, trap";
-//        LocalDate releaseAnti = LocalDate.of(2016, 2,5);
-//
-//        Song songAnti = new Song("Consideration", labelAnti, releaseAnti, genreAnti, mbidAnti, analogMediumMap4, Duration.ofMinutes(2), List.of(artistAnti));
-//
-//        Album albumAnti = new Album("ANTI",
-//                labelAnti,
-//                releaseAnti,
-//                genreAnti,
-//                mbidAnti,
-//                analogMediumMap4,
-//                List.of(songAnti));
-//
-//        em.persist(songAnti);
-//        em.persist(albumAnti);
-//
-//        //ALBUM 5
-//        Artist artistLetItBe = new Artist("The Beatles");
-//
-//        em.persist(artistLetItBe);
-//
-//        String mbidLetItBe = "12ae3a87-0ce7-416c-bb04-a1e3447031fd";
-//        String labelLetItBe = "Apple Records, Universal Music Group International";
-//        String genreLetItBe = "rock";
-//        LocalDate releaseLetItBe = LocalDate.of(2021, 10,15);
-//
-//        Song songLetItBe = new Song("Two of Us", labelLetItBe, releaseLetItBe, genreLetItBe, mbidLetItBe, analogMediumMap5, Duration.ofMinutes(3), List.of(artistLetItBe));
-//
-//        Album albumLetItBe = new Album("Let It Be",
-//                labelLetItBe,
-//                releaseLetItBe,
-//                genreLetItBe,
-//                mbidLetItBe,
-//                analogMediumMap5,
-//                List.of(songLetItBe));
-//
-//        em.persist(songLetItBe);
-//        em.persist(albumLetItBe);
-//
-//        //ALBUM 6
-//        Artist artistEverything = new Artist("Underground");
-//
-//        em.persist(artistEverything);
-//
-//        String mbidEverything = "a84dea70-f81b-4761-a39c-2dd3a9e985cc";
-//        String labelEverything = "Junior Boy's Own";
-//        String genreEverything = "techno";
-//        LocalDate releaseEverything = LocalDate.of(2000, 1,1);
-//
-//        Song songEverything = new Song("Juanita / Kiteless", labelEverything, releaseEverything, genreEverything, mbidEverything, analogMediumMap6, Duration.ofMinutes(12), List.of(artistEverything));
-//
-//        Album albumEverything = new Album("Everything, Everything",
-//                labelEverything,
-//                releaseEverything,
-//                genreEverything,
-//                mbidEverything,
-//                analogMediumMap6,
-//                List.of(songEverything));
-//
-//        em.persist(songEverything);
-//        em.persist(albumEverything);
-//
-//        //ALBUM 7
-//        Artist artistTouchBlue = new Artist("Miles Davis");
-//
-//        em.persist(artistTouchBlue);
-//
-//        String mbidTouchBlue = "283bae1f-1a6d-4ac9-80d8-68818d8016e3";
-//        String labelTouchBlue = "RevOla Records";
-//        String genreTouchBlue = "Jazz";
-//        LocalDate releaseTouchBlue = LocalDate.of(2021, 1,1);
-//
-//        Song songTouchBlue = new Song("So What?", labelTouchBlue, releaseTouchBlue, genreTouchBlue, mbidTouchBlue, analogMediumMap7, Duration.ofMinutes(9), List.of(artistTouchBlue));
-//
-//        Album albumTouchBlue = new Album("A Touch Of Blue",
-//                labelTouchBlue,
-//                releaseTouchBlue,
-//                genreTouchBlue,
-//                mbidTouchBlue,
-//                analogMediumMap7,
-//                List.of(songTouchBlue));
-//
-//        em.persist(songTouchBlue);
-//        em.persist(albumTouchBlue);
+        Artist artistAnti = new Artist("Rihanna");
+
+        String mbidAnti = "a84dea70-f81b-4761-a39c-2dd3a9e985cc";
+        String labelAnti = "Westbury Road Entertainment";
+        String genreAnti = "contemporary r&b, pop, hip hop, soul, trap";
+        LocalDate releaseAnti = LocalDate.of(2016, 2,5);
+
+        Song songAnti = new Song("Consideration", labelAnti, releaseAnti, genreAnti, "", mbidAnti, mediums4, Duration.ofMinutes(2), Set.of(artistAnti));
+
+        Album albumAnti = new Album("ANTI",
+                labelAnti,
+                releaseAnti,
+                genreAnti,
+                "",
+                mbidAnti,
+                mediums4,
+                Set.of(songAnti));
+
+        em.persist(albumAnti);
+
+        //ALBUM 5
+        Set<Medium> mediums5 = new HashSet<>();
+        mediums5.add(new Medium(BigDecimal.valueOf(19), MediumType.CD, Stock.of(Quantity.of(17)), new Supplier("ZYX Music GmbH & Co KG", Duration.ofDays(1))));
+
+        Artist artistLetItBe = new Artist("The Beatles");
+
+        String mbidLetItBe = "12ae3a87-0ce7-416c-bb04-a1e3447031fd";
+        String labelLetItBe = "Apple Records, Universal Music Group International";
+        String genreLetItBe = "rock";
+        LocalDate releaseLetItBe = LocalDate.of(2021, 10,15);
+
+        Song songLetItBe = new Song("Two of Us", labelLetItBe, releaseLetItBe, genreLetItBe, "", mbidLetItBe, mediums5, Duration.ofMinutes(3), Set.of(artistLetItBe));
+
+        Album albumLetItBe = new Album("Let It Be",
+                labelLetItBe,
+                releaseLetItBe,
+                genreLetItBe,
+                "",
+                mbidLetItBe,
+                mediums5,
+                Set.of(songLetItBe));
+
+        em.persist(albumLetItBe);
+
+        //ALBUM 6
+        Set<Medium> mediums6 = new HashSet<>();
+        mediums6.add(new Medium(BigDecimal.valueOf(19), MediumType.CD, Stock.of(Quantity.of(17)), new Supplier("ZYX Music GmbH & Co KG", Duration.ofDays(1))));
+
+        Artist artistEverything = new Artist("Underground");
+
+        String mbidEverything = "a84dea70-f81b-4761-a39c-2dd3a9e985cc";
+        String labelEverything = "Junior Boy's Own";
+        String genreEverything = "techno";
+        LocalDate releaseEverything = LocalDate.of(2000, 1,1);
+
+        Song songEverything = new Song("Juanita / Kiteless", labelEverything, releaseEverything, genreEverything, "", mbidEverything, mediums6, Duration.ofMinutes(12), Set.of(artistEverything));
+
+        Album albumEverything = new Album("Everything, Everything",
+                labelEverything,
+                releaseEverything,
+                genreEverything,
+                "",
+                mbidEverything,
+                mediums6,
+                Set.of(songEverything));
+
+        em.persist(albumEverything);
+
+        //ALBUM 7
+        Set<Medium> mediums7 = new HashSet<>();
+        mediums7.add(new Medium(BigDecimal.valueOf(19), MediumType.CD, Stock.of(Quantity.of(17)), new Supplier("ZYX Music GmbH & Co KG", Duration.ofDays(1))));
+
+        Artist artistTouchBlue = new Artist("Miles Davis");
+
+        String mbidTouchBlue = "283bae1f-1a6d-4ac9-80d8-68818d8016e3";
+        String labelTouchBlue = "RevOla Records";
+        String genreTouchBlue = "Jazz";
+        LocalDate releaseTouchBlue = LocalDate.of(2021, 1,1);
+
+        Song songTouchBlue = new Song("So What?", labelTouchBlue, releaseTouchBlue, genreTouchBlue, "", mbidTouchBlue, mediums7, Duration.ofMinutes(9), Set.of(artistTouchBlue));
+
+        Album albumTouchBlue = new Album("A Touch Of Blue",
+                labelTouchBlue,
+                releaseTouchBlue,
+                genreTouchBlue,
+                "",
+                mbidTouchBlue,
+                mediums7,
+                Set.of(songTouchBlue));
+
+        em.persist(albumTouchBlue);
 
         em.flush();
         em.getTransaction().commit();
