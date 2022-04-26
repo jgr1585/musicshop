592b7d68f63d88db710f48613f07ed23d5f2ab15
Ivo <ir-dev@reich.onl>
2022-04-26 11:41:07 +0200

    WIP message sentOnTimestamp

--- backend/src/main/java/at/fhv/teamd/musicshop/backend/domain/message/Message.java
+++ backend/src/main/java/at/fhv/teamd/musicshop/backend/domain/message/Message.java
@@ -15,17 +15,17 @@ public class Message {
 
     private Instant sentOnTimestamp;
 
-    private Message(String topicName, String title, String body) {
+    private Message(String topicName, String title, String body, Instant sentOnTimestamp) {
         this.topicName = Objects.requireNonNull(topicName);
         this.title = Objects.requireNonNull(title);
         this.body = Objects.requireNonNull(body);
     }
 
     public static Message of(String topicName, String title, String body) {
-        return new Message(topicName, title, body);
+        return new Message(topicName, title, body, null);
     }
 
-    public void markAsSent(Instant sentOnTimestamp) {
-        this.sentOnTimestamp = sentOnTimestamp;
+    public static Message of(String topicName, String title, String body, Instant sentOnTimestamp) {
+        return new Message(topicName, title, body, sentOnTimestamp);
     }
 }
