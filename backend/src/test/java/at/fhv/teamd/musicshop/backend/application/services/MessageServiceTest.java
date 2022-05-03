183f8212a48beb79dd99eb21b7a9a059a0618feb
jgr1585 <jgr1585@students.fhv.at>
2022-05-03 18:26:46 +0200

    Added get All Topic Test

--- backend/src/test/java/at/fhv/teamd/musicshop/backend/application/services/MessageServiceTest.java
+++ backend/src/test/java/at/fhv/teamd/musicshop/backend/application/services/MessageServiceTest.java
@@ -1,10 +1,36 @@
 package at.fhv.teamd.musicshop.backend.application.services;
 
+import at.fhv.teamd.musicshop.backend.domain.DomainFactory;
+import at.fhv.teamd.musicshop.backend.domain.repositories.TopicRepository;
+import at.fhv.teamd.musicshop.backend.domain.topic.Topic;
+import at.fhv.teamd.musicshop.backend.infrastructure.RepositoryFactory;
+import at.fhv.teamd.musicshop.library.DTO.TopicDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class MessageServiceTest {
+
+    @Mock
+    private TopicRepository topicRepository;
+
+    private MessageService messageService;
+
+    @BeforeEach
+    void init() {
+        RepositoryFactory.setTopicRepository(topicRepository);
+        messageService = new MessageService();
+    }
 
     @Test
     void publish() {
@@ -23,6 +49,21 @@ class MessageServiceTest {
 
     @Test
     void getAllTopics() {
-        fail("Not tested yet");
+        // given
+        Set<Topic> topics = Set.of(DomainFactory.createTopic(), DomainFactory.createTopic(), DomainFactory.createTopic());
+
+        Mockito.when(topicRepository.findAllTopics()).thenReturn(topics);
+
+        Set<TopicDTO> expectedTopics = topics.stream()
+                .map(DTOProvider::buildTopicDTO)
+                .collect(Collectors.toSet());
+
+        // when
+        Set<TopicDTO> actualTopics = messageService.getAllTopics();
+
+        // then
+        Assertions.assertEquals(expectedTopics, actualTopics);
+
+
     }
 }
