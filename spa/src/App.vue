b46a07adade285b581a6e4f4567fc2236c19b9ee
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2022-05-18 00:44:12 +0200

    [WIP] search article online

--- spa/src/App.vue
+++ spa/src/App.vue
@@ -1,20 +1,72 @@
 <script setup>
-import Search from './components/Search.vue'
+import Search from "./components/Search.vue";
+import ShoppingCart from "./components/ShoppingCart.vue";
+</script>
+
+<script>
+export default {
+  components: {
+    Search,
+    ShoppingCart
+  },
+  data() {
+    return {
+      currentTab: "Search",
+      tabs: ["Search", "ShoppingCart"],
+    };
+  },
+};
 </script>
 
 <template>
   <header>
-    <img alt="Musicshop24 logo" src="./assets/logo.png" width="320" height="230"/>
+    <img alt="Musicshop24 logo" src="./assets/logo.png" width="320" height="230" />
 
-    <!-- <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div> -->
+    <div class="demo">
+      <button
+        v-for="tab in tabs"
+        :key="tab"
+        :class="['tab-button', { active: currentTab === tab }]"
+        @click="currentTab = tab">
+        {{ tab }}
+      </button>
+      <component :is="currentTab"></component>
+    </div>
   </header>
 
-  <main>
-    <Search />
-  </main>
+  <main></main>
 </template>
 
-<!-- <style>
-</style> -->
+<style>
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+
+.tab-button {
+  padding: 6px 10px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background: #f0f0f0;
+  margin-bottom: -1px;
+  margin-right: -1px;
+}
+.tab-button:hover {
+  background: #e0e0e0;
+}
+.tab-button.active {
+  background: #e0e0e0;
+}
+.tab {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+</style>
--- spa/src/components/Search.vue
+++ spa/src/components/Search.vue
@@ -1,5 +1,75 @@
 <script setup>
+import article from "./Article.vue";
+import axios from "axios";
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: false,
+      errored: false,
+      articles: [],
+      title: "",
+      artist: "",
+    };
+  },
+  filters: {},
+  methods: {
+    searchArticles() {
+      this.loading = true;
+      axios
+        .get(
+          "http://localhost:8080/backend-1.0-SNAPSHOT/rest/article/search?title=" +
+            this.title +
+            "&artist=" +
+            this.artist
+        )
+        .then((response) => {
+          console.log(response);
+          this.articles = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally((this.loading = false));
+    },
+  },
+};
 </script>
 
 <template>
+  <header>
+    <h1>Search articles</h1>
+  </header>
+
+  <main>
+    <div>
+      <input type="text" v-model="title" placeholder="Title" />
+      <input type="text" v-model="artist" placeholder="Artist" />
+      <button @click="searchArticles">Search</button>
+    </div>
+
+    <div>
+      <section v-if="errored">
+        <p>
+          We're sorry, we're not able to retrieve this information at the moment, please
+          try back later
+        </p>
+      </section>
+
+      <section v-else>
+        <div v-if="loading">Loading...</div>
+        <div v-else v-for="article in articles">
+          <h2>Title: {{ article.title }}</h2>
+          <p>Type: {{ article.type }}</p>
+          <p>Label: {{ article.label }}</p>
+          <p>Artists: <i v-for="artist in article.artists">{{ artist.name }}; </i></p>
+          <button @click="addToCart">Add to cart</button>
+          <!-- <article :article="article"></article> -->
+        </div>
+      </section>
+    </div>
+  </main>
 </template>
