173bd53e754b769f6ad92eaf6f564e6d3e818ab2
ssa7090 <ssa7090@students.fhv.at>
2022-05-24 22:50:44 +0200

    invoice completed - mini bugs

--- backend/src/main/java/at/fhv/teamd/musicshop/backend/rest/InvoiceRestController.java
+++ backend/src/main/java/at/fhv/teamd/musicshop/backend/rest/InvoiceRestController.java
@@ -1,43 +1,142 @@
 package at.fhv.teamd.musicshop.backend.rest;
 
+import at.fhv.teamd.musicshop.backend.application.services.InvoiceService;
+import at.fhv.teamd.musicshop.backend.rest.auth.AuthenticatedUser;
 import at.fhv.teamd.musicshop.backend.rest.auth.Secured;
 import at.fhv.teamd.musicshop.backend.application.services.ServiceFactory;
-import at.fhv.teamd.musicshop.library.DTO.AlbumDTO;
-import at.fhv.teamd.musicshop.library.DTO.InvoiceDTO;
+import at.fhv.teamd.musicshop.backend.rest.auth.User;
+import at.fhv.teamd.musicshop.library.exceptions.InvoiceException;
+import at.fhv.teamd.musicshop.library.exceptions.UnauthorizedInvoiceException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import java.util.List;
 
-// TODO: Use JWT to identify user identity and remove userId params from request attributes
 @Secured
 @Path("/invoice")
 @Consumes("application/json")
 @Produces("application/json")
+@SecurityRequirement(name = "Authentication")
 public class InvoiceRestController {
+
+    @Inject
+    @AuthenticatedUser
+    private User authenticatedUser;
+
+    private InvoiceService invoiceService = ServiceFactory.getInvoiceServiceInstance();
 
     public InvoiceRestController() {}
 
     @GET
-    public List<InvoiceDTO> getInvoices(@QueryParam("customerNo") int customerNo) {
-       return ServiceFactory.getInvoiceServiceInstance().getInvoices(customerNo);
+    @Operation(summary = "Get customer invoices")
+    @ApiResponse(responseCode = "200", description = "Returns List<InvoiceDTO>")
+    @ApiResponse(responseCode = "401", description = "Unauthorized")
+    public Response getInvoices() {
+        if (authenticatedUser == null) {
+            return Response.status(401).build();
+        }
+
+        try {
+            return Response.ok(invoiceService.getInvoices(authenticatedUser.name())).build();
+        } catch (InvoiceException e) {
+            return Response.status(404, "Customer with username not found").build();
+        }
     }
 
     @GET
     @Path("/{invoiceId}")
-    public List<AlbumDTO> getInvoiceAlbums(@PathParam("invoiceId") long invoiceId) {
-        // TODO: verify that user is accessing its own invoices (customerNo)
-        return ServiceFactory.getInvoiceServiceInstance().getInvoiceAlbums(invoiceId);
+    @Operation(summary = "Get customer invoice")
+    @ApiResponse(responseCode = "200", description = "Returns List<AlbumDTO>")
+    @ApiResponse(responseCode = "401", description = "Unauthorized")
+    @ApiResponse(responseCode = "404", description = "Invoice not found")
+    public Response getInvoiceAlbums(@PathParam("invoiceId") long invoiceId) {
+        if (authenticatedUser == null) {
+            return Response.status(401).build();
+        }
+
+        try {
+            return Response.ok(invoiceService.getInvoiceAlbums(authenticatedUser.name(), invoiceId)).build();
+        } catch (UnauthorizedInvoiceException e) {
+            return Response.status(401, e.getMessage()).build();
+        } catch (InvoiceException e) {
+            return Response.status(404, e.getMessage()).build();
+        }
     }
 
     @GET
-    @Path("/{invoiceId}/download/{albumId}")
-    public String[] getInvoiceAlbumDownloadUrls(@Context UriInfo uriInfo, @PathParam("invoiceId") long invoiceId, @PathParam("albumId") long albumId) {
-        // TODO: verify that user is accessing its own invoices and downloads (customerNo)
-        String baseUri = uriInfo.getBaseUri().toString();
-        String baserUriTrailedSlash = baseUri.substring(0, baseUri.lastIndexOf('/'));
-        String baseUriWithoutApplicationPath = baserUriTrailedSlash.substring(0, baserUriTrailedSlash.lastIndexOf('/')) + '/';
-        return ServiceFactory.getInvoiceServiceInstance().getInvoiceAlbumDownloadUrls(baseUriWithoutApplicationPath, invoiceId, albumId);
+    @Path("/{invoiceId}/download-album/{albumId}")
+    @Operation(summary = "Get customer album download URLs")
+    @ApiResponse(responseCode = "200", description = "Returns String")
+    @ApiResponse(responseCode = "401", description = "Unauthorized")
+    @ApiResponse(responseCode = "404", description = "Invoice or album not found")
+    public Response getInvoiceAlbumDownloadUrl(@Context UriInfo uriInfo, @PathParam("invoiceId") long invoiceId, @PathParam("albumId") long albumId) {
+        if (authenticatedUser == null) {
+            return Response.status(401).build();
+        }
+
+        try {
+            String baseUri = uriInfo.getBaseUri().toString();
+            String baserUriTrailedSlash = baseUri.substring(0, baseUri.lastIndexOf('/'));
+            String baseUriWithoutApplicationPath = baserUriTrailedSlash.substring(0, baserUriTrailedSlash.lastIndexOf('/')) + '/';
+
+            String albumUrl = invoiceService.getInvoiceAlbumDownloadUrl(authenticatedUser.name(), baseUriWithoutApplicationPath, invoiceId, albumId);
+
+            return Response.ok(singlePropertyJson("albumUrl", albumUrl)).build();
+        } catch (UnauthorizedInvoiceException e) {
+            return Response.status(401, e.getMessage()).build();
+        } catch (InvoiceException e) {
+            return Response.status(404, e.getMessage()).build();
+        } catch (JsonProcessingException e) {
+            return Response.status(500).build();
+        }
+    }
+
+    private String singlePropertyJson(String propertyName, String value) throws JsonProcessingException {
+        // create `ObjectMapper` instance
+        ObjectMapper mapper = new ObjectMapper();
+
+        // create a JSON object
+        ObjectNode json = mapper.createObjectNode();
+        json.put(propertyName, value);
+
+        // convert `ObjectNode` to pretty-print JSON
+        // without pretty-print, use `user.toString()` method
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+    }
+
+    @GET
+    @Path("/{invoiceId}/download-song/{songId}")
+    @Operation(summary = "Get customer song download URLs")
+    @ApiResponse(responseCode = "200", description = "Returns String")
+    @ApiResponse(responseCode = "401", description = "Unauthorized")
+    @ApiResponse(responseCode = "404", description = "Invoice or song not found")
+    public Response getInvoiceSongDownloadUrl(@Context UriInfo uriInfo, @PathParam("invoiceId") long invoiceId, @PathParam("songId") long songId) {
+        if (authenticatedUser == null) {
+            return Response.status(401).build();
+        }
+
+        try {
+            String baseUri = uriInfo.getBaseUri().toString();
+            String baserUriTrailedSlash = baseUri.substring(0, baseUri.lastIndexOf('/'));
+            String baseUriWithoutApplicationPath = baserUriTrailedSlash.substring(0, baserUriTrailedSlash.lastIndexOf('/')) + '/';
+
+            String songUrl = invoiceService.getInvoiceSongDownloadUrl(authenticatedUser.name(), baseUriWithoutApplicationPath, invoiceId, songId);
+
+            return Response.ok(singlePropertyJson("songUrl", songUrl)).build();
+        } catch (UnauthorizedInvoiceException e) {
+            return Response.status(401, e.getMessage()).build();
+        } catch (InvoiceException e) {
+            return Response.status(404, e.getMessage()).build();
+        } catch (JsonProcessingException e) {
+            return Response.status(500).build();
+        }
     }
 }
--- backend/src/main/java/at/fhv/teamd/musicshop/backend/rest/MediaController.java
+++ backend/src/main/java/at/fhv/teamd/musicshop/backend/rest/MediaController.java
@@ -1,74 +1,111 @@
 package at.fhv.teamd.musicshop.backend.rest;
 
+import at.fhv.teamd.musicshop.backend.application.services.ArticleService;
+import at.fhv.teamd.musicshop.backend.application.services.InvoiceService;
 import at.fhv.teamd.musicshop.backend.application.services.ServiceFactory;
 import at.fhv.teamd.musicshop.backend.domain.article.Album;
 import at.fhv.teamd.musicshop.backend.domain.article.Song;
+import at.fhv.teamd.musicshop.backend.rest.auth.AuthenticatedUser;
+import at.fhv.teamd.musicshop.backend.rest.auth.Secured;
+import at.fhv.teamd.musicshop.backend.rest.auth.User;
 import at.fhv.teamd.musicshop.library.DTO.AlbumDTO;
 import at.fhv.teamd.musicshop.library.DTO.SongDTO;
 import at.fhv.teamd.musicshop.library.exceptions.ApplicationClientException;
+import at.fhv.teamd.musicshop.library.exceptions.InvoiceException;
+import at.fhv.teamd.musicshop.library.exceptions.UnauthorizedInvoiceException;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import java.io.*;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 @WebServlet("/media/*")
+@Secured
+@SecurityRequirement(name = "Authentication")
 public class MediaController extends HttpServlet {
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        //TODO: Check if user is logged in and has permission to access this resource
-        if (req.getPathInfo().equals("/songs")) {
-            int songId = Integer.parseInt(req.getParameter("id"));
+    @Inject
+    @AuthenticatedUser
+    private User authenticatedUser;
 
-            ServletOutputStream out = resp.getOutputStream();
-            InputStream in = getClass().getResourceAsStream("/WEB-INF/songs/" + songId + ".mp3");
-            assert in != null;
-            in.transferTo(out);
+    private InvoiceService invoiceService = ServiceFactory.getInvoiceServiceInstance();
+    private ArticleService articleService = ServiceFactory.getArticleServiceInstance();
+
+    private void serveSong(int invoiceId, int songId, HttpServletResponse resp) throws IOException, InvoiceException, UnauthorizedInvoiceException {
+        // check if is allowed to access
+        invoiceService.getSongDTO(authenticatedUser.name(), invoiceId, songId);
+
+        ServletOutputStream out = resp.getOutputStream();
+        FileInputStream in = new FileInputStream("/WEB-INF/songs/" + songId + ".mp3");
+        assert in != null;
+        in.transferTo(out);
+
+        in.close();
+        out.close();
+    }
+
+    private void serveAlbum(int invoiceId, int albumId, HttpServletResponse resp) throws ApplicationClientException, IOException, InvoiceException, UnauthorizedInvoiceException {
+        // check if is allowed to access
+        AlbumDTO album = invoiceService.getAlbumDTO(authenticatedUser.name(), invoiceId, albumId);
+
+        Set<Long> songIds = album.songs().stream()
+                .map(SongDTO::id).collect(Collectors.toSet());
+
+        ServletOutputStream out = resp.getOutputStream();
+        ZipOutputStream zip = new ZipOutputStream(out);
+
+        for (var songId : songIds) {
+            FileInputStream in = new FileInputStream("/WEB-INF/songs/" + songId + ".mp3");
+            zip.putNextEntry(new ZipEntry(songId + ".mp3"));
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                zip.write(buffer, 0, length);
+            }
 
             in.close();
-            out.close();
-        } else if (req.getPathInfo().equals("/albums")) {
-            long albumId = Integer.parseInt(req.getParameter("id"));
-            AlbumDTO album = null;
-            try {
-                album = ServiceFactory.getArticleServiceInstance().getAlbumById(albumId).orElse(null);
-            } catch (ApplicationClientException e) {
-                e.printStackTrace();
+        }
+
+        zip.close();
+        out.close();
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            int invoiceId = Integer.parseInt(req.getParameter("invoiceId"));
+
+            if (req.getPathInfo().equals("/song")) {
+                int songId = Integer.parseInt(req.getParameter("id"));
+
+                serveSong(invoiceId, songId, resp);
+            } else if (req.getPathInfo().equals("/album")) {
+                int albumId = Integer.parseInt(req.getParameter("id"));
+
+                serveAlbum(invoiceId, albumId, resp);
             }
-
-            if (album != null) {
-                Set<Long> songIds = album.songs().stream().map(SongDTO::id).collect(Collectors.toSet());
-
-                ServletOutputStream out = resp.getOutputStream();
-                ZipOutputStream zip = new ZipOutputStream(out);
-
-                songIds.forEach(songId -> {
-                    try {
-                        FileInputStream in = new FileInputStream("/WEB-INF/songs/" + songId + ".mp3");
-                        zip.putNextEntry(new ZipEntry(songId + ".mp3"));
-
-                        byte[] buffer = new byte[1024];
-                        int length;
-                        while ((length = in.read(buffer)) > 0) {
-                            zip.write(buffer, 0, length);
-                        }
-                        in.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-
-                zip.close();
-                out.close();
-            } else {
-                resp.sendError(404);
-            }
+        } catch (NumberFormatException | ApplicationClientException e) {
+            resp.sendError(400, "Invalid id.");
+        } catch (FileNotFoundException e) {
+            resp.sendError(404, "Requested file not found.");
+        } catch (IOException e) {
+            resp.sendError(500);
+        } catch (InvoiceException e) {
+            resp.sendError(404, "Invoice not found.");
+        } catch (UnauthorizedInvoiceException e) {
+            resp.sendError(401, "Unauthorized.");
         }
     }
 }
