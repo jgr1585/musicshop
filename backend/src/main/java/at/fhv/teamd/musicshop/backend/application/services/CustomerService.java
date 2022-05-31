1d95ef28884f206670b9d2b0e67fff7123c70f27
yagmursagdic <ysa1064@students.fhv.at>
2022-05-31 15:29:48 +0200

    SonarLint Code smells edited

--- backend/src/main/java/at/fhv/teamd/musicshop/backend/application/services/CustomerService.java
+++ backend/src/main/java/at/fhv/teamd/musicshop/backend/application/services/CustomerService.java
@@ -7,17 +7,12 @@ import at.fhv.teamd.musicshop.backend.infrastructure.RepositoryFactory;
 import at.fhv.teamd.musicshop.library.DTO.CustomerDTO;
 import at.fhv.teamd.musicshop.library.exceptions.CustomerDBClientException;
 import at.fhv.teamd.musicshop.library.exceptions.CustomerNotFoundException;
-import at.fhv.teamd.musicshop.library.exceptions.InvoiceException;
 
 import java.rmi.RemoteException;
 import java.util.Set;
 
 public class CustomerService {
-    private static CustomerRepository customerRepository;
-
-    public CustomerService() {
-        customerRepository = RepositoryFactory.getCustomerRepositoryInstance();
-    }
+    private static final CustomerRepository customerRepository = RepositoryFactory.getCustomerRepositoryInstance();
 
     public Set<CustomerDTO> searchCustomersByName(String name) throws RemoteException, CustomerDBClientException {
         return CustomerClient.getCustomerClient().searchCustomersByName(name);
